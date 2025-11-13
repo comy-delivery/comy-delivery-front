de07608323d390d2017c5ebffff853b5f73e22d4
Sinara-santinoni <sinara_santinoni@hotmail.com>
2025-11-13 16:40:13 -0300

    Feat: Criação do componente perfi-restaurante

--- src/app/views/perfil/perfil.ts
+++ src/app/views/perfil/perfil.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { PerfilRestaurante } from '../../components/perfil-restaurante/perfil-restaurante';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [PerfilRestaurante,CommonModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.scss',
+  
 })
 export class Perfil {
 
