d64e0f4fa948663708bac961e65548e4b827a3c7
Thurrrrr <arthur.ant.oliv@gmail.com>
2025-11-13 17:30:40 -0300

    Feat: Entregador

--- src/app/components/perfil-entregador/perfil-entregador.ts
+++ src/app/components/perfil-entregador/perfil-entregador.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { PainelEntregador } from '../painel-entregador/painel-entregador';
-import { EntregaDisponivel } from '../entrega-disponivel/entrega-disponivel';
 
 @Component({
   selector: 'app-perfil-entregador',
-  imports: [PainelEntregador, EntregaDisponivel],
+  imports: [],
   templateUrl: './perfil-entregador.html',
   styleUrl: './perfil-entregador.scss',
 })
