39817bdbef13cf62fd9c9a7b89c255d0028806d1
Sinara-santinoni <sinara_santinoni@hotmail.com>
2025-11-17 14:50:53 -0300

    fix: componente PerfilRestaurante convertido para standalone e erro NG2012 corrigido

--- src/app/components/perfil-restaurante/perfil-restaurante.ts
+++ src/app/components/perfil-restaurante/perfil-restaurante.ts
@@ -3,35 +3,25 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-perfil-restaurante',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './perfil-restaurante.html',
   styleUrl: './perfil-restaurante.scss',
 })
 export class PerfilRestaurante {
+  
+  tabAtiva: string = 'pedidos';
+
   cardData: any[] = [
-    { 
-      title: 'Novos Pedidos',
-      value: 2,
-      icon: '©',
-      iconColor: '#ffc107'
-    },
-    { 
-      title: 'Em Preparo',
-      value: 0,
-      icon: '⏲️',
-      iconColor: '#28a745'
-    },
-    { 
-      title: 'Produtos',
-      value: 3,
-      icon: '🍕',
-      iconColor: '#dc3545'
-    },
-    { 
-      title: 'Receita Hoje',
-      value: 'R$ 159,40',
-      icon: '$',
-      iconColor: 'greenyellow'
-    }
+    { title: 'Novos Pedidos', value: 2, icon: '©', iconColor: '#ffc107' },
+    { title: 'Em Preparo', value: 0, icon: '⏲️', iconColor: '#28a745' },
+    { title: 'Produtos', value: 3, icon: '🍕', iconColor: '#dc3545' },
+    { title: 'Receita Hoje', value: 'R$ 159,40', icon: '$', iconColor: 'greenyellow' }
   ];
-} 
+
+  switchTab(tab: string) {
+    this.tabAtiva = tab;
+    console.log("Tab selecionada:", tab);
+  }
+
+}
